89c8cca533da48eb1f828ab90549ae7bb7fa4592
0xEVG <evgenikrolikov@gmail.com>
2022-03-12 22:50:04 +0700

    Improved waiting page. It is now more responsive to different screen resolutions and looks much prettier. The background has also been changed.

--- src/ts/WaitingScreen.ts
+++ src/ts/WaitingScreen.ts
@@ -1,25 +1,51 @@
+
+
 export default function renderWaitingScreen() {
+    function copyText(e:Event){
+        e.preventDefault();
+        let text = link_box_input.value;
+        navigator.clipboard.writeText(text);
+    }
+
+    let black_box = document.createElement("div");
+    black_box.className = "black_box";
+
     let success_message = document.createElement('h1');
-    success_message.append("Room has been created! Here is inviting link:")
+    success_message.append("The room has been created! Here is the invitation link: ")
 
-    let link_message = document.createElement('input');
+    let link_box = document.createElement("div");
+    link_box.className = "link_box"
 
+    // let link_box_span = document.createElement('span');
+    // link_box_span.className = "link_box__url"
+
+    let link_box_input = document.createElement('input');
+    link_box_input.className = "link_box__url";
+    link_box_input.disabled = true;
+
+    let link_box_copy_icon = document.createElement('i');
+    link_box_copy_icon.className = "link_box__icon uil uil-copy"
 
     let copy_button = document.createElement('button');
-    copy_button.value = "Copy";
+    copy_button.append(link_box_copy_icon);
+    copy_button.className = "link_box__copy_button";
+    copy_button.addEventListener("click", copyText);
+
+
+
 
     let waiting_message = document.createElement('h1');
-    waiting_message.append("Waiting for second player to connect");
+    waiting_message.append("waiting for second player to connect");
 
-    let div = document.createElement("div");
     return{
         render:function (root: HTMLElement, url: string){
-            link_message.value = url;
-            div.append(success_message,link_message,copy_button,waiting_message)
-            root.append(div)
+            link_box_input.value = url;
+            link_box.append(link_box_input,copy_button);
+            black_box.append(success_message,link_box,waiting_message);
+            root.append(black_box)
         },
         destroy: function (){
-            div.remove();
+            black_box.remove();
         }
     }
 }
